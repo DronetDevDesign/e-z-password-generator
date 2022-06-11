256f5570320d6b7baaf1e1f7e16099f60d66a44d
Ron Dronet <ron@dronetdevdesign.com>
2022-06-11 09:47:21 -0500

    revised Math.random() lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,92 +14,46 @@ function writePassword() {
 // *-------- all code goes below this comment --------*
 
 function generatePassword() {
-  window.confirm("Click OK is you're ready to start the process!");
 
-  var userCharNumber = window.prompt(
-    "How many characters would you like your password to contain?"
-  );
-  if (userCharNumber >= 8 && userCharNumber <= 128) {
-    console.log(userCharNumber);
-  } else {
-    window.alert(
-      "Password must be AT LEAST 8 characters and NO MORE THAN 128 characters in length"
-    );
-  }
+// input for number of characters
+  var userCharNumber = window.prompt("How many characters would you like your password to contain?");
+  if (userCharNumber >= 8 && userCharNumber <= 128) 
+  {console.log(userCharNumber);
+// alert is characters below 8 or above 128
+  } else {window.alert("Password must be AT LEAST 8 characters and NO MORE THAN 128 characters in length");
+    generatePassword();
+  };
 
-  var specialCharArray = window.confirm(
-    "Click OK to confirm inlcuding special characters"
-  );
-  specialCharArray = [
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "()",
-    "*",
-    "+",
-    "-",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-  ];
-  specialCharArray[Math.floor(Math.random() * specialCharArray.length)];
-  console.log(specialCharArray);
+// confirm for special characters
+  var specialChar = window.confirm("Click OK to confirm inlcuding special characters");
+  specialCharArray = ["!", "#", "$", "%", "&", "()", "*", "+", "-", "<", "=", ">", "?", "@"];
+  specialChar = specialCharArray[Math.floor(Math.random() * specialCharArray.length)];
+  console.log(specialChar);
 
-  var numericCharArray = window.confirm(
-    "Click OK to confirm including numeric characters"
-  );
+// confirm for numeric characters
+  var numericChar = window.confirm("Click OK to confirm including numeric characters");
   numericCharArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  numericCharArray[Math.floor(Math.random() * numericCharArray.length)];
-  console.log(numericCharArray);
+  numericChar = numericCharArray[Math.floor(Math.random() * numericCharArray.length)];
+  console.log(numericChar);
 
-  var charArray = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
+// array for alphabets
+  var charArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  var lowerCaseChars = window.confirm(
-    "Click OK to confirm lowercase characters"
-  );
+// confirm for lowercase characters
+  var lowerCaseChars = window.confirm("Click OK to confirm lowercase characters");
   lowerCaseChars = charArray.toString().toLowerCase();
   console.log(lowerCaseChars);
 
-  var upperCaseChars = window.confirm(
-    "Click OK to confirm uppercase characters"
-  );
+// confirm for uppercase characters
+  var upperCaseChars = window.confirm("Click OK to confirm uppercase characters");
   upperCaseChars = charArray.toString().toUpperCase();
   console.log(upperCaseChars);
 
   // if (!specialCharArray && !numericCharArray && !lowerCaseChars && !upperCaseChars) {
   //   window.alert("You must select at least one character type!");
   // };
+
+// contain in local storage
   var password = localStorage.getItem("password");
 }
 
